0fdfef41287b06e72b82fc437119fb496319c14c
Kimi Nel <kimi.monique.nel@gmail.com>
2023-07-26 11:53:45 +0200

    feat: Content Update

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,6 @@ import React from "react";
 const About = () => {
   return (
     <div className="w-full pt-28 pb-4 px-4 md:px-24">
-      <div className="flex flex-col rounded-xl p-4 shadow-xl gap-y-4 bg-white">
         <div className="">
           <h2 className="text-sm md:text-[1.8rem] font-bold pb-4 md:leading-8">
             About me
@@ -12,13 +11,13 @@ const About = () => {
             I am a BEd graduate (with distinction) who provides a supportive,
             fun, playful, warm, loving, inspiring and safe learning and play
             environment (using my first hand mind of a toddler experience!)
-            Since I was 10 years old, all I ever wanted was to be a mother. I
-            love to hike and spend quality time with friends. As a mom, I enjoy
-            spoiling my daughter and taking her on adventures to amusement
-            parks, the aquarium, beaches, petting zoos and gathering places such
-            as Ipic Play in Aurora, etc!
+            Since I was 10 years old, all I ever wanted was to be a mother. As a
+            mom, I enjoy spoiling my daughter and taking her on adventures to
+            amusement parks, the aquarium, beaches, petting zoos and gathering
+            places such as Ipic Play in Aurora, etc!
           </p>
         </div>
+      <div className="flex flex-col rounded-xl p-4 shadow-xl gap-y-4 bg-white">
 
         <div className="">
           <h2 className="text-sm md:text-[1.8rem] font-bold pb-4 md:leading-8">
@@ -44,17 +43,16 @@ const About = () => {
           <ul className="text-xs md:text-lg">
             <li>Children can sense the care, warmth and understanding</li>
             <li>
-              There is patience, love, where children feel safe, happy at school
+              There is patience and love, so that children feel safe and happy
             </li>
             <li>
               Happy chatter, giggles, play and friendship are the memories
             </li>
             <li>
-              Foundations of a love for learning is fostered, where learning is
-              fun
+              Foundation of a Love for Learning is fostered, making learning fun
             </li>
             <li>
-              There is no favouritism, where no-one feels out or looked down on
+              There is no favouritism and no-one feels out or looked down upon
             </li>
           </ul>
         </div>
