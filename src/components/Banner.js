8deeb3c5c0bdbc86f63ba6be0e96e74e860a1879
Kimi Nel <kimi.monique.nel@gmail.com>
2023-07-26 13:27:57 +0200

    style: Home page Banner motion

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AnimatedText } from "./AnimatedText";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
@@ -8,12 +9,15 @@ const Banner = () => {
         text={"Welcome to Chevon's Tutoring Services!"}
         className="text-darkPurple pb-8 text-center"
       />
-      <div className="w-2/3 bg-white rounded-2xl shadow-xl py-8 px-12 text-center">
-        <p className="text-lg md:text-[1.8rem] text-purple font-bold pb-4 leading-2 md:leading-8">
+      <motion.div
+        className="w-5/6 md:w-2/3 bg-white rounded-2xl shadow-xl py-8 px-12 text-center"
+        whileHover={{ scale: 1.1 }}
+      >
+        <p className="text-xl md:text-[1.8rem] text-purple font-bold pb-4 md:leading-8">
           Are you ready to embark on an exciting journey of learning and
           discovery?
         </p>
-        <p className="text-xs md:text-lg">
+        <p className="text-sm md:text-lg">
           Hi! I'm Chevon - your friendly and dedicated tutor, here to make
           learning a breeze for students from Grades 4 to 7. I believe that
           every subject is an adventure waiting to be explored! My tailored
@@ -22,7 +26,7 @@ const Banner = () => {
           learning together, where education becomes an enjoyable and rewarding
           experience. Get ready to shine like never before!
         </p>
-      </div>
+      </motion.div>
     </section>
   );
 };
